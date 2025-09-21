8117d82f886d66f42a025abbab8c2e5a9da45152
Rudramani Dhiman <154111820+Rudraam@users.noreply.github.com>
2025-09-21 14:56:17 -0400

    Lab Exercise 3

---
+++ week03_exercise/index.js
@@ -0,0 +1,40 @@
+var http = require("http");
+const employees = require('./Employee');
+console.log("Lab 03 -  NodeJs");
+
+const port = process.env.PORT || 8082
+
+const server = http.createServer((req, res) => {
+    if (req.method !== 'GET') {
+        res.end(`{"error": "${http.STATUS_CODES[405]}"}`)
+    } else {
+        if (req.url === '/') {
+            res.setHeader('Content-Type', 'text/html');
+            res.end('<h1>Welcome to Lab Exercise 03</h1>');
+        }
+
+        else if (req.url === '/employee') {
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(employees));
+        }
+
+        else if (req.url === '/employee/names') {
+            const names = employees.map(emp => `${emp.firstName} ${emp.lastName}`).sort();
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(names));
+        }
+
+        else if (req.url === '/employee/totalsalary') {
+            const totalSalary = employees.reduce((sum, emp) => sum + emp.Salary, 0);
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({ "total_salary": totalSalary }));
+        }
+        else {
+            res.end(`{"error": "${http.STATUS_CODES[404]}"}`)
+        }
+    }
+})
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+})
